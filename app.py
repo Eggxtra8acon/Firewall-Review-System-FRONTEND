c345b88b83856d2559a522c4d8d3c410fc2aa1c5
Isaiah Rafailes <isaiahgabrielrafailes@gmail.com>
2026-04-24 23:51:08 +0800

    sprint-1-wip-start

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route('/dashboard') 
+@app.route('/') 
 def dashboard():
     return render_template('dashboard.html')
 
